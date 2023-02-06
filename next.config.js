47d9076089d9d28f0bfc3eca1da536e182db3fda
Caio Duarte Santos <caio.duarte@dtidigital.com.br>
2023-02-05 23:56:49 -0300

    habilitando edge

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { runtime: 'experimental-edge' },
   images: {
     domains: [process.env.WP_IMAGES_URI],
   },
