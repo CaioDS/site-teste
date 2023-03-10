d7235bafda4085ce1b76619ec2c808acdd38ef26
Caio Duarte Santos <caio.duarte@dtidigital.com.br>
2023-03-10 09:56:33 -0300

    Allow vercel experimental-edge

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   images: {
     domains: [process.env.WP_IMAGES_URI],
