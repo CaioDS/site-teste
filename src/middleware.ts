7d0ceabfc4b64cae6fbf69273c506ed29a81cc4a
SquadSites <squad.sitesdti@dtidigital.com.br>
2023-05-05 12:30:07 -0300

    Teste

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,36 @@ enum Pages {
   FAVICON = '/favicon.ico',
 }
 
+function isImageURI(pageKey: string) {
+  return (
+    pageKey.includes('.png') ||
+    pageKey.includes('.jpg') ||
+    pageKey.includes('.jpeg') ||
+    pageKey.includes('.svg') ||
+    pageKey.includes('.webp') ||
+    pageKey.includes('.gif') ||
+    pageKey.includes('.tiff') ||
+    pageKey.includes('.bmp') ||
+    pageKey.includes('.psd')
+  );
+}
+
+function isNextPageURI(pageKey: string) {
+  return (
+    pageKey !== Pages.HOME &&
+    pageKey !== Pages.METHODOLOGY &&
+    pageKey !== Pages.FAVICON &&
+    !pageKey.includes(Pages.NEXT_PAGES)
+  );
+}
+
 export function middleware(req: NextRequest) {
   console.log('Pagina', req.nextUrl.pathname);
   const pageKey: string = req.nextUrl.pathname;
+
+  if (!isNextPageURI(pageKey) && isImageURI(pageKey)) {
+    return `${process.env.TARGET_URI}${pageKey}`;
+  }
 
   if (
     pageKey !== Pages.HOME &&
